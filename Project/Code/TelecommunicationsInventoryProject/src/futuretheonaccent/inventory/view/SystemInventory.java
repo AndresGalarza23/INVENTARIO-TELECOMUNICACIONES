540f5abd4deeb8910aac2695ee30b0acf64b1298
Stefany1-Guerrero <guerrerostefy7@gmail.com>
2021-06-24 18:50:29 -0500

    update

---
+++ Project/Code/TelecommunicationsInventoryProject/src/futuretheonaccent/inventory/view/SystemInventory.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package futuretheonaccent.inventory.view;
+
+/**
+ *
+ * @author Stefany Guerrero AccentOnTheFuture ESPE-DCC0
+ */
+public class SystemInventory {
+    public static void main(String[] args) {
+        
+        
+        
+    }
+}
