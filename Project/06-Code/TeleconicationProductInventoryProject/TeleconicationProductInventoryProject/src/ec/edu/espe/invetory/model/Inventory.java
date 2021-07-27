1760977158960ee1303b18dff3acd796e6334d43
Haro Morales luis <84456145+LuisHaro95@users.noreply.github.com>
2021-07-27 10:10:05 -0500

    inventoryClass

--- Project/06-Code/TeleconicationProductInventoryProject/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/model/Inventory.java
+++ Project/06-Code/TeleconicationProductInventoryProject/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/model/Inventory.java
@@ -43,6 +43,46 @@ public class Inventory {
         this.id = id;
     }
     
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+    
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+    
+    public double getSalePrice() {
+        return salePrice;
+    }
+    
+    public void setSalePrice(double salePrice) {
+        this.salePrice = salePrice;
+    }
+    
+    public int getQuantity() {
+        return quantity;
+    }
+    
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
     
     
 }
