ac580ecdd1dda3e2b7546c6469db8c1379d1a09e
Haro Morales luis <84456145+LuisHaro95@users.noreply.github.com>
2021-07-27 11:41:02 -0500

    providerClass

--- Project/06-Code/TeleconicationProductInventoryProject/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/model/Provider.java
+++ Project/06-Code/TeleconicationProductInventoryProject/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/model/Provider.java
@@ -26,6 +26,29 @@ public class Provider {
         this.address = direction;
     }
      
+    public String getId() {
+        return id;
+    }
+    
+    public void setId(String id) {
+        this.id = id;
+    }
+    
+    public String getName() {
+        return name;
+    }
+    
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+     public String getPhoneNumber() {
+        return phoneNumber;
+    }
+     
+     public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
      
     
 }
