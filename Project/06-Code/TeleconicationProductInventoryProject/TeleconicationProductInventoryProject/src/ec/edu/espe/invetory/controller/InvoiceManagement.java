bffb3f1c48a28e32deca18f7582fa60fd9807e26
Alberto Heredia Iza <laheredia3@espe.edu.ec>
2021-08-01 20:50:41 -0500

    InvoiceController

--- Project/06-Code/TeleconicationProductInventoryProject/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/controller/InvoiceManagement.java
+++ Project/06-Code/TeleconicationProductInventoryProject/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/controller/InvoiceManagement.java
@@ -5,13 +5,27 @@
  */
 package ec.edu.espe.invetory.controller;
 
+import ec.edu.espe.invetory.model.Invoice;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.Scanner;
+
 /**
  *
  * @author Luis Heredia Accent on the Future ESPE-DCC0
  */
 public class InvoiceManagement {
     
-    
+     public void inputInvoice(JSONObject object, int quantity) throws IOException, ParseException, org.json.simple.parser.ParseException {
+        String data = FileManager.read("data/Invoice.json");
+        Invoice invoice = new Invoice();
+        JSONArray array = (JSONArray) new JSONParser().parse(data);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter ID Invoice");
+        String id = sc.nextLine();
+        invoice.setId(id);
+        
+        
     
     
     
