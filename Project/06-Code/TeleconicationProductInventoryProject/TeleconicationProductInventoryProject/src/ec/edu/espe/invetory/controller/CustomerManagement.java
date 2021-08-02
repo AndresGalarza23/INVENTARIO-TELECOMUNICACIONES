ae6a19e75f9766a6751a3a1258d6c2c600f4f547
Stefany1-Guerrero <guerrerostefy7@gmail.com>
2021-08-01 19:22:37 -0500

    JsonCostomer

--- Project/06-Code/TeleconicationProductInventoryProject/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/controller/CustomerManagement.java
+++ Project/06-Code/TeleconicationProductInventoryProject/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/controller/CustomerManagement.java
@@ -5,10 +5,46 @@
  */
 package ec.edu.espe.invetory.controller;
 
+import java.io.IOException;
+import java.text.ParseException;
+
 /**
  *
  * @author Stefany Guerrero AccentOnTheFuture ESPE-DCC0
  */
 public class CustomerManagement {
-    
+   
+     public JSONArray inputCustomer() throws IOException, ParseException {
+        String data = FileManager.read("client.json");
+        Scanner sc = new Scanner(System.in);
+        JSONArray array = new JSONArray();
+        JSONParser parser = new JSONParser();
+        Customer customer = new Customer();
+        array = (JSONArray) parser.parse(data);
+
+        System.out.println("Enter Id");        
+        String id = sc.nextLine();
+        customer.setCedula(id);
+        
+        System.out.println("Enter Name");
+        String name = sc.nextLine();
+        customer.setNames(name);
+        
+        System.out.println("Enter Last Name");
+        String lName = sc.nextLine();
+        customer.setLastName(lName);
+        
+        System.out.println("Enter Adress");
+        String adress = sc.nextLine();
+        customer.setAddress(adress);        
+
+        System.out.println("Enter Phone");
+        String phone = sc.nextLine();
+        customer.setPhone(phone); 
+     }
+     
+     
+     
+     
+     
 }
