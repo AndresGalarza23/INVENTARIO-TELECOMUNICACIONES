534ae4cf0898bdc82720e78490a89878fd1370c0
Alberto Heredia Iza <laheredia3@espe.edu.ec>
2021-08-03 03:43:21 -0500

    JsonReadSaleManagement

--- Project/06-Code/TeleconicationProductInventoryProject/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/controller/SaleManagement.java
+++ Project/06-Code/TeleconicationProductInventoryProject/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/controller/SaleManagement.java
@@ -5,10 +5,44 @@
  */
 package ec.edu.espe.invetory.controller;
 
+import ec.edu.espe.invetory.model.Inventory;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  *
  * @author Luis Heredia Accent on the Future ESPE-DCC0
  */
 public class SaleManagement {
+    
+    
+    public void sale() throws IOException, ParseException, java.text.ParseException {
+        InvoiceManagement invoice = new InvoiceManagement();
+        Scanner sc = new Scanner(System.in);
+        String data = FileManager.read("data/Inventory.json");
+        Inventory inventory = new Inventory();
+        JSONArray array = (JSONArray) new JSONParser().parse(data);
+        JSONObject jsonObject = new JSONObject();
+        boolean flag = false;
+        
+        
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
