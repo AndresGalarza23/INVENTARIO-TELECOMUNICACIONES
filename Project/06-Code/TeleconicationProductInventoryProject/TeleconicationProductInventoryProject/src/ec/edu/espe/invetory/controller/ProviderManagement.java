49eb5e4ac31440e0c9ee0aa9060b6951ee92f836
Stefany1-Guerrero <guerrerostefy7@gmail.com>
2021-08-02 23:23:00 -0500

    editProviderM

--- Project/06-Code/TeleconicationProductInventoryProject/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/controller/ProviderManagement.java
+++ Project/06-Code/TeleconicationProductInventoryProject/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/controller/ProviderManagement.java
@@ -5,6 +5,8 @@
  */
 package ec.edu.espe.invetory.controller;
 
+import java.util.Date;
+
 
 
 /**
@@ -18,9 +20,18 @@ public class ProviderManagement {
         Provider provider = new Provider();
         JSONArray array = (JSONArray) new JSONParser().parse(data);
         Scanner sc = new Scanner(System.in);
+        System.out.println("Enter ID Invoice");
+        String id = sc.nextLine();
+        invoice.setId(id);
+
+        Date date = new Date();
+        System.out.println(date);
+        invoice.setDate(date.toString());
     
-    
-    
+        System.out.println("Enter Cedula Customer");
+        String cedulaCustomer = sc.nextLine();
+        invoice.setCedulaClient(cedulaCustomer);
+        
     
     
     
