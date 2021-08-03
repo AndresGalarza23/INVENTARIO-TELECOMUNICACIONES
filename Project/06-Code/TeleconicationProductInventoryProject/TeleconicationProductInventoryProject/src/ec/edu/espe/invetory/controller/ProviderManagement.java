aa0ad9f46deb632a82f040b3c9c54532866f860c
Alberto Heredia Iza <laheredia3@espe.edu.ec>
2021-08-03 03:34:37 -0500

    Creat SaleManagement and Correction class ProviderManagement

--- Project/06-Code/TeleconicationProductInventoryProject/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/controller/ProviderManagement.java
+++ Project/06-Code/TeleconicationProductInventoryProject/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/controller/ProviderManagement.java
@@ -27,41 +27,15 @@ import org.json.simple.parser.JSONParser;
 public class ProviderManagement {
     public void inputProvider() throws IOException, ParseException, org.json.simple.parser.ParseException {
     
-     String data = FileManager.read("data/Provider.json");
+      public void inputProvider() throws IOException, ParseException, org.json.simple.parser.ParseException {
+        String data = FileManager.read("data/Provider.json");
         Provider provider = new Provider();
         JSONArray array = (JSONArray) new JSONParser().parse(data);
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter ID Invoice");
+        System.out.println("Enter Id");
         String id = sc.nextLine();
-        invoice.setId(id);
-
-        Date date = new Date();
-        System.out.println(date);
-        invoice.setDate(date.toString());
-    
-        System.out.println("Enter Cedula Customer");
-        String cedulaCustomer = sc.nextLine();
-        invoice.setCedulaClient(cedulaCustomer);
+        provider.setId(id);
         
-        System.out.println("Enter Tax");
-        double tax = sc.nextDouble();
-        invoice.setTax(tax);
-
-        double subTotal = (quantity * Double.parseDouble(object.get("salePrice").toString()));
-        double total = subTotal + subTotal*(tax / 100);
-
-        invoice.setTotal(total);
-        System.out.println("Total to pay: "+ total);
-        JSONObject invoiceJson = new JSONObject();
-        invoiceJson.put("id", invoice.getId());
-        invoiceJson.put("cedulaCustomer", invoice.getCedulaClient());
-        invoiceJson.put("tax", invoice.getTax());
-        invoiceJson.put("date", invoice.getDate());
-        invoiceJson.put("total", invoice.getTotal());
-       array.add(invoiceJson);
-
-        FileManager.writeRecord("data/Invoice.json", array.toJSONString());
-    
     
     
     
