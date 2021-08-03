8ff673e546fba7bcd585feca327ccf10f09bfbbb
Alberto Heredia Iza <laheredia3@espe.edu.ec>
2021-08-03 03:37:33 -0500

    Correction class ProviderManagement

--- Project/06-Code/TeleconicationProductInventoryProject/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/controller/ProviderManagement.java
+++ Project/06-Code/TeleconicationProductInventoryProject/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/controller/ProviderManagement.java
@@ -6,17 +6,13 @@
 package ec.edu.espe.invetory.controller;
 
 import ec.edu.espe.filemanagerlibrary.FileManager;
-
-import ec.edu.espe.invetory.model.Invoice;
 import ec.edu.espe.invetory.model.Provider;
 import java.io.IOException;
 import java.text.ParseException;
-import java.util.Date;
 import java.util.Scanner;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
-
 
 
 
@@ -25,7 +21,7 @@ import org.json.simple.parser.JSONParser;
  * @author Stefany Guerrero AccentOnTheFuture ESPE-DCC0
  */
 public class ProviderManagement {
-    public void inputProvider() throws IOException, ParseException, org.json.simple.parser.ParseException {
+    
     
       public void inputProvider() throws IOException, ParseException, org.json.simple.parser.ParseException {
         String data = FileManager.read("data/Provider.json");
@@ -35,6 +31,34 @@ public class ProviderManagement {
         System.out.println("Enter Id");
         String id = sc.nextLine();
         provider.setId(id);
+        
+         System.out.println("Enter Name");
+        String name = sc.nextLine();
+        provider.setName(name);
+
+        System.out.println("Enter Adress");
+        String adress = sc.nextLine();
+        provider.setAddress(adress);
+
+        System.out.println("Enter Phone");
+        String phone = sc.nextLine();
+        provider.setPhoneNumber(phone);
+
+        JSONObject providerJson = new JSONObject();
+        providerJson.put("id", provider.getId());
+        providerJson.put("name", provider.getName());
+        providerJson.put("adress", provider.getAddress());
+        providerJson.put("phone", provider.getPhoneNumber());
+        array.add(providerJson);
+        FileManager.writeRecord("data/Provider.json", array.toJSONString());
+    }
+        
+        
+        
+        
+        
+        
+        
         
     
     
