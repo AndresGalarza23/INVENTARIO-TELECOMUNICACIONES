6d67e5434a546af4fcda2ee25e5961bd4877be99
Alberto Heredia Iza <laheredia3@espe.edu.ec>
2021-08-03 23:53:06 -0500

    Create ProductWindow and Complete program

--- Project/06-Code/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/view/InventorySystem.java
+++ Project/06-Code/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/view/InventorySystem.java
@@ -5,6 +5,8 @@
  */
 package ec.edu.espe.invetory.view;
 
+
+
 /**
  *
  * @author Luis Heredia Accent on the Future ESPE-DCC0
@@ -27,10 +29,12 @@ public class InventorySystem extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         btmProduct = new javax.swing.JButton();
         btmProvider = new javax.swing.JButton();
-        btmCustomer = new javax.swing.JButton();
+        btmSale = new javax.swing.JButton();
+        btmExit = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -51,51 +55,85 @@ public class InventorySystem extends javax.swing.JFrame {
             }
         });
 
-        btmCustomer.setText("CUSTOMER");
+        btmSale.setText("SALE");
+
+        btmExit.setText("EXIT");
+        btmExit.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btmExitActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap(186, Short.MAX_VALUE)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(197, 197, 197))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(btmProvider, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btmSale, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btmExit, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(btmProduct, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addGap(226, 226, 226))))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel1)
+                .addGap(49, 49, 49)
+                .addComponent(btmProduct)
+                .addGap(40, 40, 40)
+                .addComponent(btmProvider)
+                .addGap(36, 36, 36)
+                .addComponent(btmSale)
+                .addGap(29, 29, 29)
+                .addComponent(btmExit)
+                .addContainerGap(67, Short.MAX_VALUE))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addGap(126, 126, 126)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(btmCustomer, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(btmProduct, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(btmProvider, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(137, 137, 137))
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(25, 25, 25)
-                .addComponent(jLabel1)
-                .addGap(49, 49, 49)
-                .addComponent(btmProduct)
-                .addGap(18, 18, 18)
-                .addComponent(btmProvider)
-                .addGap(18, 18, 18)
-                .addComponent(btmCustomer)
-                .addContainerGap(91, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void btmProductActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btmProductActionPerformed
-
+  
         ProductWindow prodW = new ProductWindow();
         prodW.setVisible(true);
-
+        
+       
+        
+        
+        
     }//GEN-LAST:event_btmProductActionPerformed
 
     private void btmProviderActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btmProviderActionPerformed
-
-        ProviderWindow provW = new ProviderWindow();
-        provW.setVisible(true);
-
+      
+      ProviderWindow provW = new ProviderWindow();
+        provW.setVisible(true); 
+        
+        
     }//GEN-LAST:event_btmProviderActionPerformed
+
+    private void btmExitActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btmExitActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_btmExitActionPerformed
 
     /**
      * @param args the command line arguments
@@ -123,6 +161,7 @@ public class InventorySystem extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(InventorySystem.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -133,9 +172,11 @@ public class InventorySystem extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btmCustomer;
+    private javax.swing.JButton btmExit;
     private javax.swing.JButton btmProduct;
     private javax.swing.JButton btmProvider;
+    private javax.swing.JButton btmSale;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
 }
