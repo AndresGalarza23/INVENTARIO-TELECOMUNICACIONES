5d46c79dbbaa5aa7d4e236655a181d7dbbbbf642
Stefany1-Guerrero <guerrerostefy7@gmail.com>
2021-08-04 00:08:30 -0500

    ClassProviderContructor

--- Project/06-Code/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/model/Provider.java
+++ Project/06-Code/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/model/Provider.java
@@ -10,5 +10,79 @@ package ec.edu.espe.invetory.model;
  * @author Stefany Guerrero AccentOnTheFuture ESPE-DCC0
  */
 public class Provider {
+      private Integer id;
+    private String name;
+    private Integer phoneNumber;
+    private String address;
+
+    public Provider(Integer id, String name, Integer phoneNumber, String address) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.address = address;
+    }
+    
+    
+    
+    
+
+    /**
+     * @return the id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the phoneNumber
+     */
+    public Integer getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    /**
+     * @param phoneNumber the phoneNumber to set
+     */
+    public void setPhoneNumber(Integer phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    
+    
     
 }
