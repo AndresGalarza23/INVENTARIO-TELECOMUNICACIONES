0ec54f164af3c6e0247681f23bf7208631c4c12e
Alberto Heredia Iza <laheredia3@espe.edu.ec>
2021-08-03 23:05:17 -0500

    Method to add in MongoDB (ProductController)

--- Project/06-Code/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/controller/ProductController.java
+++ Project/06-Code/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/controller/ProductController.java
@@ -9,9 +9,12 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.Mongo;
+import ec.edu.espe.invetory.model.Product;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JOptionPane;
 
 /**
  *
@@ -36,5 +39,20 @@ public class ProductController {
     }
     
     
-    
+     public boolean add(Integer id, String name, String brand, double purchasePrice, double salePrice, Integer quantity, Integer idProvider ){
+        ArrayList<Product> inventory = new ArrayList<>();
+        inventory.add(new Product(id, name, brand, purchasePrice, salePrice, quantity, idProvider));
+        for (Product pueC : inventory){
+            collection.insert(pueC.dbProductObjectInventory());
+            
+        }
+        
+        int input = JOptionPane.showConfirmDialog(null, "Successful Registration","OK", JOptionPane.DEFAULT_OPTION);
+        
+        System.out.println(input);
+        return true;
+
+       }
+     
+     
 }
