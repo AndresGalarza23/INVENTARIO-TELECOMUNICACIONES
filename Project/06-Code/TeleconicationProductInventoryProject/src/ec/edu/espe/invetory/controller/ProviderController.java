27c317f793b8c3880519b2167c6beef5f7434575
Stefany1-Guerrero <guerrerostefy7@gmail.com>
2021-08-04 00:26:22 -0500

    MethodToAdd

--- Project/06-Code/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/controller/ProviderController.java
+++ Project/06-Code/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/controller/ProviderController.java
@@ -5,10 +5,56 @@
  */
 package ec.edu.espe.invetory.controller;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
+import ec.edu.espe.invetory.model.Provider;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Stefany Guerrero AccentOnTheFuture ESPE-DCC0
  */
 public class ProviderController {
+    
+     DB DataBase;
+    DBCollection collection;
+    BasicDBObject document = new BasicDBObject();
+    
+    public ProviderController(){
+        
+        try{
+            Mongo mongo = new Mongo("localhost", 27017);
+            DataBase = mongo.getDB("InventoryProduct");
+            collection = DataBase.getCollection("Provider");
+            System.out.println("successful connection");
+        }catch(UnknownHostException ex){
+            Logger.getLogger(ProductController.class.getName()).log(Level.SEVERE, null, ex);            
+        }
+   
+    }
+    
+     public boolean add(Integer id, String name, Integer phoneNumber, String address ){
+        ArrayList<Provider> provider = new ArrayList<>();
+        provider.add(new Provider(id, name, phoneNumber, address));
+        for (Provider pueC : provider){
+            collection.insert(pueC.dbProductObjectProvider());
+            
+        }
+        
+        int input = JOptionPane.showConfirmDialog(null, "Successful Registration","OK", JOptionPane.DEFAULT_OPTION);
+        
+        System.out.println(input);
+        return true;
+    }
+    
+    
+    
+    
     
 }
