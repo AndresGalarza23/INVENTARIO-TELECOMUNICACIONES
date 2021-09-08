15bdfd2be0d70fde40b16bacf29fd39ee5854a0f
Stefany1-Guerrero <guerrerostefy7@gmail.com>
2021-09-07 19:14:13 -0500

    Create method search Cedula

--- Project/06-Code/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/view/SearchInvoiceIdWindow.java
+++ Project/06-Code/TeleconicationProductInventoryProject/src/ec/edu/espe/invetory/view/SearchInvoiceIdWindow.java
@@ -9,12 +9,12 @@ package ec.edu.espe.invetory.view;
  *
  * @author Stefany Guerrero AccentOnTheFuture ESPE-DCC0
  */
-public class SearchInvoiceWindow extends javax.swing.JFrame {
+public class SearchInvoiceIdWindow extends javax.swing.JFrame {
 
     /**
      * Creates new form SearchInvoiceWindow
      */
-    public SearchInvoiceWindow() {
+    public SearchInvoiceIdWindow() {
         initComponents();
     }
 
@@ -105,20 +105,21 @@ public class SearchInvoiceWindow extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SearchInvoiceWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SearchInvoiceIdWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SearchInvoiceWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SearchInvoiceIdWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SearchInvoiceWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SearchInvoiceIdWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SearchInvoiceWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(SearchInvoiceIdWindow.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new SearchInvoiceWindow().setVisible(true);
+                new SearchInvoiceIdWindow().setVisible(true);
             }
         });
     }
